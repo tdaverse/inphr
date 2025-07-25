cb9be341ea3de07fa088d0380a7040828331e218
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2025-07-25 17:01:01 +0200

    Improved doc.

--- R/two-sample-test.R
+++ R/two-sample-test.R
@@ -34,11 +34,11 @@
 #'   the permutation hypothesis test. Defaults to `1000L`.
 #' @param stat_functions A list of functions that compute test statistics to be
 #'   used for solving the inference problem. These functions must take two
-#'   arguments: First, an object of class `dist` representing a distance matrix
-#'   and second, an integer value specifying the size of the first sample.
-#'   Defaults to `list(flipr::stat_t_ip, flipr::stat_f_ip`)` which are distance-
-#'   based statistics equivalent to Student's and Fisher's statistics
-#'   respectively.
+#'   arguments: first, an object of class `dist` representing a distance matrix
+#'   and second, an integer vector specifying the indices of the data points
+#'   belonging to the first sample. Defaults to
+#'   `list(flipr::stat_t_ip, flipr::stat_f_ip`)` which are distance-based
+#'   statistics equivalent to Student's and Fisher's statistics respectively.
 #' @param npc A string specifying the non-parametric combination method to use.
 #'   Choices are either `"tippett"` (default) or `"fisher"`. The former
 #'   corresponds to the Tippet's method, while the latter corresponds to
@@ -47,10 +47,19 @@
 #'   generation. Defaults to `NULL` which uses current time.
 #' @param verbose A boolean value indicating whether to print some information
 #'   about the progress of the computation. Defaults to `FALSE`.
+#' @param keep_null_distribution A boolean specifying whether the empirical
+#'   permutation null distribution should be returned as well. Defaults to
+#'   `FALSE`.
+#' @param keep_permutations A boolean specifying whether the list of sampled
+#'   permutations used to compute the empirical permutation null distribution
+#'   should be returned as well. Defaults to `FALSE`.
 #'
 #' @returns A numeric value storing the p-value from the two-sample test where
 #'   the null hypothesis is that the two samples come from the same
-#'   distribution.
+#'   distribution. If one of `keep_null_distribution` or `keep_permutations` is
+#'   set to `TRUE`, then the output will be a list containing the p-value and
+#'   the null distribution (if `keep_null_distribution` is set to `TRUE`) and
+#'   the list of sampled permutations (if `keep_permutations` is set to `TRUE`).
 #'
 #' @export
 #' @examples
@@ -66,9 +75,13 @@ two_sample_test <- function(
   stat_functions = list(flipr::stat_t_ip, flipr::stat_f_ip),
   npc = "tippett",
   seed = NULL,
-  verbose = FALSE
+  verbose = FALSE,
+  keep_null_distribution = FALSE,
+  keep_permutations = FALSE
 ) {
-  if (verbose) cli::cli_alert_info("Parsing inputs...")
+  if (verbose) {
+    cli::cli_alert_info("Parsing inputs...")
+  }
   l <- parse_inputs(
     x = x,
     y = y,
@@ -78,7 +91,9 @@ two_sample_test <- function(
   )
   D <- l$D
   sample_sizes <- l$sample_sizes
-  if (verbose) cli::cli_alert_info("Setting up the plausibility function...")
+  if (verbose) {
+    cli::cli_alert_info("Setting up the plausibility function...")
+  }
   # We could use alternative statistics for PH vectorizations
   pf <- flipr::PlausibilityFunction$new(
     null_spec = null_spec,
@@ -91,6 +106,12 @@ two_sample_test <- function(
   pf$alternative <- "right_tail"
   pf$nperms <- B
   pf$aggregator <- npc
-  if (verbose) cli::cli_alert_info("Calculating the p-value...")
-  pf$get_value(c(0, 1))
+  if (verbose) {
+    cli::cli_alert_info("Calculating the p-value...")
+  }
+  pf$get_value(
+    parameters = c(0, 1),
+    keep_null_distribution = keep_null_distribution,
+    keep_permutations = keep_permutations
+  )
 }
--- data-raw/wip-power.R
+++ data-raw/wip-power.R
@@ -4,20 +4,20 @@ R <- 100L
 B <- 100L
 alpha <- 0.05
 
-n_vec <- seq(5, 30, by = 5)
+Ns <- seq(5, 35, by = 5)
 mirai::daemons(5)
 
 withr::with_seed(1234, {
-  power_vec <- purrr::map_dbl(n_vec, \(n) {
-    cli::cli_alert_info("Running power simulation for n = {n} ...")
-    pvalues <- purrr::map_dbl(
+  cli::cli_h1("Power simulation for Archimedian spirals differing in variance")
+
+  powers_variance <- purrr::map(Ns, \(N) {
+    cli::cli_alert_info("Running power simulation for n = {N} ...")
+    pvalues <- purrr::map(
       1:R,
       purrr::in_parallel(
         \(r) {
-          cli::cli_alert_success("Simulation run {r} of {R} ...")
-
           archspirals1 <- phutil::as_persistence_set(lapply(
-            seq(n),
+            seq(N),
             function(i) {
               S1 <- tdaunif::sample_arch_spiral(
                 n = 120L,
@@ -34,7 +34,7 @@ withr::with_seed(1234, {
           ))
 
           archspirals2 <- phutil::as_persistence_set(lapply(
-            seq(n),
+            seq(N),
             function(i) {
               S2 <- tdaunif::sample_arch_spiral(
                 n = 120L,
@@ -50,17 +50,125 @@ withr::with_seed(1234, {
             }
           ))
 
-          inphr::two_sample_test(archspirals1, archspirals2, B = B)
+          list(
+            mean_variance_tippett = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_t_ip, flipr::stat_f_ip),
+              npc = "tippett"
+            ),
+            mean_variance_fisher = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_t_ip, flipr::stat_f_ip),
+              npc = "fisher"
+            ),
+            mean_only = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_t_ip)
+            ),
+            variance_only = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_f_ip)
+            )
+          )
         },
         B = B,
         R = R,
-        n = n
+        N = N
       )
-    )
-    mean(pvalues <= alpha)
-  })
+    ) |>
+      purrr::list_transpose() |>
+      purrr::map_dbl(\(p) mean(p <= alpha))
+  }) |>
+    do.call(rbind, args = _)
+
+  cli::cli_h1("Power simulation for Archimedian spirals differing in mean")
+
+  powers_mean <- purrr::map(Ns, \(N) {
+    cli::cli_alert_info("Running power simulation for n = {N} ...")
+    pvalues <- purrr::map(
+      1:R,
+      purrr::in_parallel(
+        \(r) {
+          archspirals1 <- phutil::as_persistence_set(lapply(
+            seq(N),
+            function(i) {
+              S1 <- tdaunif::sample_arch_spiral(
+                n = 120L,
+                arms = 2L,
+                ar = 1,
+                sd = 0.05
+              )
+              phutil::as_persistence(TDA::ripsDiag(
+                S1,
+                maxdimension = 2,
+                maxscale = 6
+              ))
+            }
+          ))
+
+          archspirals2 <- phutil::as_persistence_set(lapply(
+            seq(N),
+            function(i) {
+              S2 <- tdaunif::sample_arch_spiral(
+                n = 120L,
+                arms = 2L,
+                ar = 1.1,
+                sd = 0.05
+              )
+              phutil::as_persistence(TDA::ripsDiag(
+                S2,
+                maxdimension = 2,
+                maxscale = 6
+              ))
+            }
+          ))
+
+          list(
+            mean_variance_tippett = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_t_ip, flipr::stat_f_ip),
+              npc = "tippett"
+            ),
+            mean_variance_fisher = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_t_ip, flipr::stat_f_ip),
+              npc = "fisher"
+            ),
+            mean_only = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_t_ip)
+            ),
+            variance_only = inphr::two_sample_test(
+              archspirals1,
+              archspirals2,
+              B = B,
+              stat_functions = list(flipr::stat_f_ip)
+            )
+          )
+        },
+        B = B,
+        R = R,
+        N = N
+      )
+    ) |>
+      purrr::list_transpose() |>
+      purrr::map_dbl(\(p) mean(p <= alpha))
+  }) |>
+    do.call(rbind, args = _)
 })
 
 mirai::daemons(0)
-
-plot(n_vec, power_vec, type = "b")
