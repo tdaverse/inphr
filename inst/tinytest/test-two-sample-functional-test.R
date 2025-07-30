dd9a81cee0e265fd5e2529d8a2e272875026657f
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2025-07-30 16:25:52 +0200

    Bring coverage to 100%.

--- inst/tinytest/test-two-sample-functional-test.R
+++ inst/tinytest/test-two-sample-functional-test.R
@@ -5,6 +5,52 @@ spl2 <- phutil::as_persistence_set(trefoils2[1:5])
 expect_error(two_sample_functional_test(1, spl2))
 expect_error(two_sample_functional_test(spl1, 1))
 
-# Test correct behavior
-out <- two_sample_functional_test(spl1, spl2, scale_size = 50L, B = 100L)
+# Test correct behavior with betti curve
+out <- two_sample_functional_test(
+  spl1,
+  spl2,
+  scale_size = 50L,
+  B = 100L,
+  representation = "betti"
+)
+expect_length(out, 4L)
+
+# Test correct behavior with euler characteristic
+out <- two_sample_functional_test(
+  spl1,
+  spl2,
+  scale_size = 50L,
+  B = 100L,
+  representation = "euler"
+)
+expect_length(out, 4L)
+
+# Test correct behavior with life curve
+out <- two_sample_functional_test(
+  spl1,
+  spl2,
+  scale_size = 50L,
+  B = 100L,
+  representation = "life"
+)
+expect_length(out, 4L)
+
+# Test correct behavior with silhouette
+out <- two_sample_functional_test(
+  spl1,
+  spl2,
+  scale_size = 50L,
+  B = 100L,
+  representation = "silhouette"
+)
+expect_length(out, 4L)
+
+# Test correct behavior with entropy
+out <- two_sample_functional_test(
+  spl1,
+  spl2,
+  scale_size = 50L,
+  B = 100L,
+  representation = "entropy"
+)
 expect_length(out, 4L)
